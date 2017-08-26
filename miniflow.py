Logit is after log(softmax) applied; correct misnaming

miniflow.py
@@ -89,9 +89,9 @@ class CategoricalCrossentropy(Layer):
         self.grad[self.pred] = - self.grad_cost * self.true.value / self.pred.value
         self.grad[self.true] = - self.grad_cost * np.log(self.pred.value)
 
-class CategoricalCrossentropyWithLogit(Layer):
+class CategoricalCrossentropyWithSoftmax(Layer):
     def __init__(self, pred, true):
-        super(CategoricalCrossentropyWithLogit, self).__init__([pred, true])
+        super(CategoricalCrossentropyWithSoftmax, self).__init__([pred, true])
         self.pred = pred
         self.true = true
 
@@ -100,13 +100,9 @@ class CategoricalCrossentropyWithLogit(Layer):
         self.value = -np.mean(self.true.value * np.log(self.softmax))
 
     def backward(self):
-        super(CategoricalCrossentropyWithLogit, self).backward()
+        super(CategoricalCrossentropyWithSoftmax, self).backward()
         self.grad[self.pred] = self.grad_cost * (self.softmax - self.true.value)
         self.grad[self.true] = self.grad_cost * np.log(self.softmax)
-
-        # # (N, n)
-        # self.grad[self.pred] = - self.grad_cost * self.true.value / self.pred.value
-        # self.grad[self.true] = - np.log(self.pred.value)
 
 class Sigmoid(Layer):
     def __init__(self, x):

nn.py
@@ -25,7 +25,7 @@ W, b = mf.Input(), mf.Input()
 
 linear = mf.Linear(X, W, b)
 # activation = mf.Softmax(linear)
-cost = mf.CategoricalCrossentropyWithLogit(linear, Y)
+cost = mf.CategoricalCrossentropyWithSoftmax(linear, Y)
 
 def get_batches(x, y, batch_size):
     num_samples = len(x)
@@ -38,7 +38,7 @@ def get_batches(x, y, batch_size):
             yield x[keys], y[keys]
 
 epochs = 100
-batch_size = 2
+batch_size = 100
 steps_per_epoch = int(np.ceil(len(x_train) / float(batch_size)))
 W_ = np.random.randn(28*28, num_classes)
 b_ = np.random.randn(num_classes)